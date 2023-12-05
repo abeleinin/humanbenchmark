ef40badf4140350796ad02941c95d12b32af4fb8
Abe Leininger <95333017+abeleinin@users.noreply.github.com>
2023-12-04 23:52:12 -0500

    firebase env auth

--- firebase.tsx
+++ firebase.tsx
@@ -4,23 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // "AIzaSyD9_bmknXxHpkbPy2Qq0a38hYsaJLeXVHs",
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// }
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD9_bmknXxHpkbPy2Qq0a38hYsaJLeXVHs",
-  authDomain: "brain-games-758a6.firebaseapp.com",
-  projectId: "brain-games-758a6",
-  storageBucket: "brain-games-758a6.appspot.com",
-  messagingSenderId: "839928296527",
-  appId: "1:839928296527:web:c9e58c3afef660fc19e63a",
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 
